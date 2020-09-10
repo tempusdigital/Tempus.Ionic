eaed3bfdc857562f18ddd9d802ebde10b7366b08
Francisco Fuzari <franciscofuzari@tempus.digital>
2020-09-10 16:30:11 -0300

    componente t-pager adicionado, página de teste do t-pager criada, adicionado link para a página de teste no index.html

---
+++ src/components/t-pager-popover/t-pager-popover.tsx
@@ -0,0 +1,44 @@
+import { Component, h, Prop } from '@stencil/core';
+import { PagerButton, Messages } from '../t-pager/interfaces';
+import { popoverController } from '@ionic/core';
+
+@Component({
+  tag: 't-pager-popover',
+})
+
+export class TPagerPopover {
+
+  @Prop() messages: Messages = {
+
+    nextPage: 'Próxima',
+    previousPage: 'Anterior',
+    firstPage: 'Primeira Página',
+    lastPage: 'Ultima Página',
+
+  }
+
+  handleButtonClick(button: PagerButton) {
+
+    popoverController.dismiss({ button });
+
+  }
+
+  render() {
+
+    return [
+
+      <ion-list class="ion-no-padding">
+
+        <ion-item button lines="full" onClick={() => this.handleButtonClick('first-page')}>
+          <ion-label>{this.messages.firstPage}</ion-label>
+        </ion-item>
+
+        <ion-item button lines="none" onClick={() => this.handleButtonClick('last-page')}>
+          <ion-label>{this.messages.lastPage}</ion-label>
+        </ion-item>
+
+      </ion-list>
+
+    ];
+  }
+}
